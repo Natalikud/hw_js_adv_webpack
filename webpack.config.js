61f8b5f1a0c547a0257c49519ac36b00d1aa1323
Natalikud2 <Reimova091@mail.ru>
2023-07-14 11:52:21 +0800

    Version 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
+    entry: './src/index.js',
     
-}
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader, 'css-loader',
+            ],
+          },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin ( {
+            template: './src/index.html',
+            filename: 'main.html'
+        }),
+        new MiniCssExtractPlugin ( {
+            filename: 'main.css'
+        }),
+    ],
+
+};
+
+
+
